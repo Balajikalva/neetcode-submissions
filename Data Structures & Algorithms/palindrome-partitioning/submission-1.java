a3a711fc18579a76139af0c515e9416732243e5e
Balajikalva <51396354+Balajikalva@users.noreply.github.com>
2026-05-11 17:27:05 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.java
@@ -0,0 +1,80 @@
+class Solution {
+
+    boolean[][] dp;
+    List<List<String>> sols = new ArrayList<>();
+
+    public List<List<String>> partition(String s) {
+
+        int n = s.length();
+        
+        this.dp = new boolean[n][n];
+
+        for (int i = 0;i < s.length(); i++) {
+
+            matchTillPalindrome(s, i, i);
+            matchTillPalindrome(s, i, i + 1);
+        }
+
+        for (int i = 0;i < dp.length; i++) {
+
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        helper(0, s, new ArrayList<>());
+
+        return sols;
+
+    }
+
+
+    private void helper(int ind, String s, List<String> selections) {
+
+        if (ind == s.length()) {
+
+            sols.add(new ArrayList<>(selections));
+
+            return;
+        }
+
+        for (int i = ind; i < s.length(); i++) {
+
+            if (dp[ind][i] != true) continue;
+
+            String sub = s.substring(ind, i + 1);
+
+            selections.add(sub);
+            helper(i + 1, s, selections);
+
+            selections.remove(selections.size() - 1);
+        }
+
+        return;
+    }
+
+    private boolean matchTillPalindrome(String s, int ind1, int ind2) {
+
+        // boolean canPrint = ind1 == ind2;
+        // canPrint &= ind1 == 1;
+
+        while (ind1 >= 0 && ind2 < s.length()) {
+
+            // if (canPrint) {
+            //     System.out.println(ind1+ " >> " + ind2);
+            // }
+
+            if (s.charAt(ind1) == s.charAt(ind2)) {
+
+                dp[ind1][ind2] = true;
+
+                ind1--;
+                ind2++;
+
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
